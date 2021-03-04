03932f129f08a497373373df3d96bfcf61604d87
unknown <danyllo106@gmail.com>
2021-03-04 14:01:06 -0300

    update

--- src/utils/componentes.js
+++ src/utils/componentes.js
@@ -185,19 +185,19 @@ export function Menu(props) {
   return (
     <div className="menuContainer">
       <div className="appBar">
-        <Link to="/EstoqueElox-Web/" className={location.pathname === "/EstoqueElox-Web/" ? "selected" : null}>
+        <Link to="EstoqueElox-Web/" className={location.pathname === "/EstoqueElox-Web/" ? "selected" : null}>
           <FaBox size={16} fill={"#aaa"} />
           <p>Estoque</p>
         </Link>
-        <Link to="/EstoqueElox-Web/Bateria" className={location.pathname === "/EstoqueElox-Web/Bateria" ? "selected" : null}>
+        <Link to="EstoqueElox-Web/Bateria" className={location.pathname === "/EstoqueElox-Web/Bateria" ? "selected" : null}>
           <FaCarBattery size={16} fill={"#aaa"} />
           <p>Bateria</p>
         </Link>
-        <Link to="/EstoqueElox-Web/Sucata" className={location.pathname === "/EstoqueElox-Web/Sucata" ? "selected" : null}>
+        <Link to="EstoqueElox-Web/Sucata" className={location.pathname === "/EstoqueElox-Web/Sucata" ? "selected" : null}>
           <FaDolly size={16} fill={"#aaa"} />
           <p>Sucata</p>
         </Link>
-        <Link to="/EstoqueElox-Web/#" className={location.pathname === "/EstoqueElox-Web/Logs" ? "selected" : null}>
+        <Link to="EstoqueElox-Web/#" className={location.pathname === "/EstoqueElox-Web/Logs" ? "selected" : null}>
           <FaCommentAlt size={16} fill={"#aaa"} />
           <p>Logs</p>
         </Link>
